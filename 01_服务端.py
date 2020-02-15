12aa04b3cdc51fdacff67615bad9e16a7a214514
binyushu <binyushu@qq.com>
2020-02-15 20:08:26 +0800

    修改完善

--- 01_服务端.py
+++ 01_服务端.py
@@ -1,2 +1,21 @@
 # -*-coding:utf-8-*-
 
+import socket
+
+server = socket.socket()
+server.bind(("0.0.0.0", 8080))
+server.listen()
+new_socket, new_addr = server.accept()
+
+
+
+while True:
+    new_socket.send("欢迎来到服务端".encode("utf-8"))
+    tmp_data = new_socket.recv(1024)
+    print(tmp_data.decode("utf-8"))
+    input_data = input()
+    new_socket.send(input_data.encode("utf-8"))
+
+
+# print(data)
+# new_socket.close()
--- 02_客户端.py
+++ 02_客户端.py
@@ -3,7 +3,16 @@
 import socket
 
 clinet = socket.socket()
-clinet.connect(("192.168.1.1", 8080))
-data = input("请输入你要发送的数据：\n")
-clinet.send(data.encode("utf-8"))
-clinet.close()
+clinet.connect(("127.0.0.1", 8080))
+server_data = clinet.recv(1024)
+print("服务端：{}".format(server_data.decode("utf-8")))
+
+
+while True:
+    input_data = input("请输入要输入的数据：\n")
+    clinet.send(input_data.encode("utf-8"))
+    server_data = clinet.recv(1024)
+    print("服务端：{}".format(server_data.decode("utf-8")))
+    
+
+# clinet.close()
